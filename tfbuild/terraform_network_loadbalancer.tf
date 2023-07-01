df459c8fc6d099851de97fd3b389f9590eda8101
gcsgroup-pvt-ltd <gcsgroup.pvt.ltd@gmail.com>
2023-07-01 23:19:26 +0530

    AWS CICD Setup

--- tfbuild/terraform_network_loadbalancer.tf
+++ tfbuild/terraform_network_loadbalancer.tf
@@ -21,9 +21,3 @@ resource "aws_internet_gateway" "my_internet_gateway" {
     Name = var.internet_gateway_name
   }
 }
-
-# Attach the internet gateway to the VPC
-resource "aws_vpc_attachment" "my_vpc_attachment" {
-  vpc_id             = aws_vpc.my_vpc.id
-  internet_gateway_id = aws_internet_gateway.my_internet_gateway.id
-}
