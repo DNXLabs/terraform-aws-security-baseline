fd92a1bbee63d9fb9316d83a1e52d3ccf5d081c4
gcsgroup-pvt-ltd <gcsgroup.pvt.ltd@gmail.com>
2023-07-01 22:53:40 +0530

    AWS CICD Setup

--- tfbuild/terraform_s3_bucket.tf
+++ tfbuild/terraform_s3_bucket.tf
@@ -17,14 +17,6 @@ resource "aws_s3_bucket" "s3_bucket" {
     prevent_destroy = false  # Set to true to prevent accidental bucket deletion
   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"  # Use server-side encryption with AES256
-      }
-    }
-  }
-
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET", "PUT"]
