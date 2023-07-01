0b699184ae1cccf1749caf4333be32ff723bddcc
gcsgroup-pvt-ltd <gcsgroup.pvt.ltd@gmail.com>
2023-07-01 16:09:39 +0530

    AWS CICD Setup

---
+++ modules/codebuild/main.tf
@@ -0,0 +1,34 @@
+#This solution, non-production-ready template describes AWS Codepipeline based CICD Pipeline for terraform code deployment.
+#© 2023 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
+#This AWS Content is provided subject to the terms of the AWS Customer Agreement available at
+#http://aws.amazon.com/agreement or other written agreement between Customer and either
+#Amazon Web Services, Inc. or Amazon Web Services EMEA SARL or both.
+
+resource "aws_codebuild_project" "terraform_codebuild_project" {
+
+  count = length(var.build_projects)
+
+  name           = "${var.project_name}-${var.build_projects[count.index]}"
+  service_role   = var.role_arn
+  encryption_key = var.kms_key_arn
+  tags           = var.tags
+  artifacts {
+    type = var.build_project_source
+  }
+  environment {
+    compute_type                = var.builder_compute_type
+    image                       = var.builder_image
+    type                        = var.builder_type
+    privileged_mode             = true
+    image_pull_credentials_type = var.builder_image_pull_credentials_type
+  }
+  logs_config {
+    cloudwatch_logs {
+      status = "ENABLED"
+    }
+  }
+  source {
+    type      = var.build_project_source
+    buildspec = "./templates/buildspec_${var.build_projects[count.index]}.yml"
+  }
+}
